52049f4f451b53ec55193f98022129057c8766d6
florencioeu <robson.fb10@gmail.com>
2025-05-29 19:06:29 -0300

    correção do menu-center responsivo

--- sobrenos.php
+++ sobrenos.php
@@ -11,8 +11,8 @@ $fotoPerfil = $logado && !empty($_SESSION['usuario']['foto']) ? $_SESSION['usuar
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sobre Nós</title>
   <script src="JS/carrinho.js" defer></script>
-  <script src="JS/senhac.js"></script> 
-  <script src="JS/banco_pedidos.js"></script>
+  <script src="JS/senhac.js" defer></script> 
+  <script src="JS/banco_pedidos.js" defer></script>
   <link rel="stylesheet" href="css/sobrenos.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
   
